d4d71b3330e021053d70a3d3eb8c11db2f97b400
drikara <ouattara17drissa@gmail.com>
2025-08-07 03:40:31 +0000

    j'ai appris à créer un ensemble en python

---
+++ ensembles.py
@@ -0,0 +1,10 @@
+""" 
+un ensemble est une collection de données non ordonnée sans elements en double, 
+il permet de realiser des tests d'appartenance
+
+
+"""
+
+ensemble = {"jean", "philippe","ali","mamane"}
+print(ensemble)
+    
